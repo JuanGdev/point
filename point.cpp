de6d32c00c0f896df1be947553b3881c56803822
JuanGdev <j.aguilerahuerta@ugto.mx>
2021-09-06 14:41:43 -0500

    Inicio del repositorio

---
+++ point.cpp
@@ -0,0 +1,6 @@
+#include <point.hpp>
+
+void func1()
+{
+  std::cout << "valor de pi = " << PI << std::endl;
+}
---
+++ point.hpp
@@ -0,0 +1,7 @@
+#ifndef POINT_HPP
+#define POINT_HPP
+
+#include <iostream>
+#define PI 3.1416
+
+#endif
